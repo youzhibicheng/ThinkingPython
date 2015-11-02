c0e9386b5c6261f002c84f7e6151a1aac5283776
James ZOU <youzhibicheng@163.com>
2015-11-02 13:40:31 +0800

    chapter 2 commit

--- TheHackerGuideToPython/ch03/prerequisites.sh
+++ TheHackerGuideToPython/ch03/prerequisites.sh
@@ -1,4 +1,18 @@
 !/bin/bash
 
 # install Sphinx
+sudo pip install sphinx
+# already included in pythonix
+sphinx-quickstart
 
+# understand reST
+# http://zh-sphinx-doc.readthedocs.org/en/latest/rest.html
+# http://jwch.sdut.edu.cn/book/tool/UseSphinx.html
+# http://jwch.sdut.edu.cn/book/rst.html
+
+# doctest
+# Documentation-Driven Developent DDD
+
+# readthedocs
+# automatically deploy the document
+# https://github.com/rtfd/readthedocs.org
